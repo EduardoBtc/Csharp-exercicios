93d20f22ee49476bbb044c5ddcd35f5b3c56c257
Eduardo Martins <88164806+EduardoBtc@users.noreply.github.com>
2023-10-20 20:03:18 -0300

    Entrada e saida de dados

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Globalization;
 
 namespace PrimeiroProjeto
@@ -7,7 +8,27 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Entre com seu nome completo:");
+            string namePerson = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int badroomQuantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto:");
+            double productValue = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre seu último nome, idade e altura (mesma linha):");
+            string[] nameAgeHeight = Console.ReadLine().Split(" ");
+            string lastName = nameAgeHeight[0];
+            int age = int.Parse(nameAgeHeight[1]);
+            double height = double.Parse(nameAgeHeight[2]);
+
+            Console.WriteLine(namePerson);
+            Console.WriteLine(badroomQuantity);
+            Console.WriteLine(productValue.ToString(CultureInfo.InvariantCulture));
+            Console.WriteLine(lastName);
+            Console.WriteLine(age);
+            Console.WriteLine(height.ToString(CultureInfo.InvariantCulture));
         }
     }
 }
