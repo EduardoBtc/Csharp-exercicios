2bb8843c439da6dea46557a995fa4e81d775a4a9
Eduardo Martins <88164806+EduardoBtc@users.noreply.github.com>
2023-10-20 20:31:24 -0300

    Calculando a area de um circulo

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -5,9 +5,18 @@ namespace PrimeiroProjeto
 {
     internal class Program
     {
+        private static readonly double piValue = 3.14159;
+
         static void Main(string[] args)
         {
-            
+
+            Console.WriteLine("Vamos somar a área de um circulo");
+            Console.WriteLine("Entre com o raio desse circulo");
+            double circleRadius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+             
+            double areaValue = piValue * (Math.Pow(circleRadius, 2));
+
+            Console.WriteLine($"O valor da área é :{areaValue.ToString("F4",CultureInfo.InvariantCulture)}");
         }
     }
 }
