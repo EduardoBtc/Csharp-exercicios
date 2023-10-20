5ebdbab44684bdaa340940f2a5e8bf2d2420ad2a
Eduardo Martins <88164806+EduardoBtc@users.noreply.github.com>
2023-10-20 20:08:26 -0300

    Somando dois numeros inteiros

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -7,7 +7,18 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Vamos calcular a soma dos números de entrada");
+
+            Console.WriteLine("Digite o primeiro número");
+            int numberOne = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o primeiro número");
+            int numberSecond = int.Parse(Console.ReadLine());
+
+            int numberSum = numberOne + numberSecond;
+
+            Console.WriteLine($"A soma dos números é: {numberSum}");
+
         }
     }
 }
