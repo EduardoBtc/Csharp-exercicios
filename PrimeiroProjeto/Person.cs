76cc05470809c68deceb94bfe81b1d7e426efbe2
Eduardo Martins <88164806+EduardoBtc@users.noreply.github.com>
2023-10-21 18:23:10 -0300

    Programa usando criando a entidade/classe Person que representa uma pessoa.

---
+++ PrimeiroProjeto/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace PrimeiroProjeto
+{
+    internal class Person
+    {
+        public string Name;
+        public int Age;
+    }
+}
--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -7,7 +7,31 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            
+            Person firstPerson, secondPerson;
+            firstPerson = new Person();
+            secondPerson = new Person();
+
+            Console.WriteLine("Dados da primeira pessoa:");
+
+            Console.Write("Nome: ");
+            firstPerson.Name = Console.ReadLine();
+
+            Console.Write("Idade: ");
+            firstPerson.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+
+            Console.Write("Nome: ");
+            secondPerson.Name = Console.ReadLine();
+
+            Console.Write("Idade: ");
+            secondPerson.Age = int.Parse(Console.ReadLine());
+
+            string olderPersonName = firstPerson.Age > secondPerson.Age 
+                    ? firstPerson.Name 
+                    : secondPerson.Name;
+
+            Console.WriteLine($"Pessoa mais velha: {olderPersonName}");
         }
     }
 }
