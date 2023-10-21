87a94513c272d2ef1f204a846b5d2f0c3ad147ec
Eduardo Martins <88164806+EduardoBtc@users.noreply.github.com>
2023-10-20 23:38:31 -0300

    Calcular a diferença entre números inteiros

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -7,7 +7,23 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            
+
+            Console.WriteLine("Vamos calcular a diferença entre os quatro números inseridos");
+            Console.WriteLine("Digite o primeiro número");
+            int numberFirst = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número");
+            int numberSecond = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o terceiro número");
+            int numberthree = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o quarto número");
+            int numberFor = int.Parse(Console.ReadLine());
+
+            int difference = (numberFirst * numberSecond - numberthree * numberFor);
+
+            Console.WriteLine($"A diferença é: {difference}");
         }
     }
 }
