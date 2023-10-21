3465f69f7713fc75926e56cee60ed4385a99dcd1
Eduardo Martins <88164806+EduardoBtc@users.noreply.github.com>
2023-10-21 18:40:17 -0300

    Projeto usando a entidade/classe Employee

---
+++ PrimeiroProjeto/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace PrimeiroProjeto
+{
+    internal class Employee
+    {
+        public string Name;
+        public double Salary;
+    }
+}
--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -7,7 +7,29 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            
+            Employee firstEmployee, secondEmployee;
+            firstEmployee = new Employee();
+            secondEmployee = new Employee();
+
+            Console.WriteLine("Dados do primeiro funcionário:");
+
+            Console.Write("Nome: ");
+            firstEmployee.Name = Console.ReadLine();
+
+            Console.Write("Salário: ");
+            firstEmployee.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do segundo funcionário:");
+
+            Console.Write("Nome: ");
+            secondEmployee.Name = Console.ReadLine();
+
+            Console.Write("Salário: ");
+            secondEmployee.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double averageSalary = (firstEmployee.Salary + secondEmployee.Salary) / 2.0 ;
+
+            Console.WriteLine($"Salário médio = {averageSalary.ToString("F2", CultureInfo.InvariantCulture)}");
         }
     }
 }
