1f275915e0fa87d6904cf6e5f7bbff18863a2860
Eduardo Martins <88164806+EduardoBtc@users.noreply.github.com>
2023-10-22 17:50:32 -0300

    Desenvolvimento para resolver o desafio do readme

---
+++ PrimeiroProjeto/Bank.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto
+{
+    internal class Bank
+    {
+        private double _balance;
+        private readonly double _withdrawalFee = 5.00;
+        public int BankNumber { get; private set; }
+        public string BankPersonName { get; set; }
+        public double InitialDepository { get; private set; }
+
+        public Bank()
+        {
+
+        }
+
+        public Bank(int bankNumber, string bankPersonName, double initialDepository)
+        {
+            this.BankNumber = bankNumber;
+            this.BankPersonName = bankPersonName;
+            AddBalance(initialDepository);
+        }
+
+        public Bank(int bankNumber, string bankPersonName)
+        {
+            this.BankNumber = bankNumber;
+            this.BankPersonName = bankPersonName;
+        }
+
+        public void AddBalance(double valueDepositiry)
+        {
+            this._balance += valueDepositiry;
+        }
+
+        public void RemoveBalance(double valueRemove)
+        {
+            this._balance -= (valueRemove + this._withdrawalFee);
+        }
+
+        public override string ToString()
+        {
+            return $"Conta: {this.BankNumber}, " +
+                $"Titular: {this.BankPersonName}, " +
+                $"Saldo: $ {this._balance.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -7,7 +7,48 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            
+            Bank bank = new Bank();
+
+            Console.Write("Entre o número da conta: ");
+            int bankNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string bankPersonName = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            string hasInitialDepository = Console.ReadLine();
+
+            if (hasInitialDepository.Equals("s"))
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double initialDepository = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+               bank = new Bank(bankNumber, bankPersonName, initialDepository);
+            }
+
+            if (hasInitialDepository.Equals("n"))
+                bank = new Bank(bankNumber, bankPersonName);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da conta: ");
+            Console.WriteLine(bank);
+
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para depósito: ");
+            bank.AddBalance(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(bank);
+
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para saque: ");
+            bank.RemoveBalance(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(bank);
         }
     }
 }
