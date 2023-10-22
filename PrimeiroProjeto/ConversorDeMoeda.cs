25038bda59e6b8a91e07c5f3d503e079c92b530c
Eduardo Martins <88164806+EduardoBtc@users.noreply.github.com>
2023-10-22 00:37:54 -0300

    Faça um programa para ler a cotação do dólar, e depois um valor em dólares a ser comprado por uma pessoa em reais. Informar quantos reais a pessoa vai pagar pelos dólares, considerando ainda que a pessoa terá que pagar 6% de IOF sobre o valor em dólar. Criar uma classe ConversorDeMoeda para ser responsável pelos cálculos.

---
+++ PrimeiroProjeto/ConversorDeMoeda.cs
@@ -0,0 +1,14 @@
+﻿namespace PrimeiroProjeto
+{
+    internal class ConversorDeMoeda
+    {
+        private static double taxIOF = 6.0;
+
+        public static double ConvertDolarToReal(double dolarPrice, double value) 
+        {
+            double total = value * dolarPrice;
+            return total + total * taxIOF / 100.0;
+        }
+
+    }
+}
--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -7,7 +7,15 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Qual é a cotação do dólar? ");
+            double dolarPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dólares você vai comprar? ");
+            double dolarQuantity = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Valor a ser pago em reais = " +
+                    $"{ConversorDeMoeda.ConvertDolarToReal(dolarPrice, dolarQuantity).ToString("F2", CultureInfo.InvariantCulture)}"
+                );
         }
     }
 }
