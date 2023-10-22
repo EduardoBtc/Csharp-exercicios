76e21e4a0b0ae330b9de6e8ed502d8b5fdf6f5ea
Eduardo Martins <88164806+EduardoBtc@users.noreply.github.com>
2023-10-21 23:04:12 -0300

    Fazer um programa para ler os dados de um funcionário (nome, salário bruto e imposto). Em seguida, mostrar os dados do funcionário (nome e salário líquido). Em seguida, aumentar o salário do funcionário com base em uma porcentagem dada (somente o salário bruto é afetado pela porcentagem) e mostrar novamente os dados do funcionário. Use a classe projetada abaixo

---
+++ PrimeiroProjeto/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace PrimeiroProjeto
+{
+    internal class Employee
+    {
+        public string Name;
+        public double GrossSalary;
+        public double GovernmentTax;
+
+        public double SalarioLiquido()
+        {
+            return (this.GrossSalary - this.GovernmentTax);
+        }
+
+        public void AumentarSalario(double percentage)
+        {
+            this.GrossSalary += this.GrossSalary / percentage;
+        }
+
+        public override string ToString()
+        {
+            return this.Name + ", $ " + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -7,7 +7,24 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            
+            Employee employee = new Employee();
+
+            Console.Write("Nome: ");
+            employee.Name = Console.ReadLine();
+
+            Console.Write("Salário bruto: ");
+            employee.GrossSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Imposto: ");
+            employee.GovernmentTax = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Funcionário: {employee}");
+
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            employee.AumentarSalario(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine($"Dados atualizados: {employee}");
+
         }
     }
 }
