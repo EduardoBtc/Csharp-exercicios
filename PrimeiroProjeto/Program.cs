f5a446139924f4a20ba9214fe567582babcdd75a
Eduardo Martins <88164806+EduardoBtc@users.noreply.github.com>
2023-10-21 22:42:03 -0300

    Programa para calcular Area, perimetro e diagonal de um retangulo.

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Globalization;
 
 namespace PrimeiroProjeto
@@ -7,7 +8,15 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            
+            Rectangle rectangle = new Rectangle();
+
+            Console.WriteLine("Entre a largura e altura do retângulo:");
+            rectangle.Width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            rectangle.Height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"AREA = {rectangle.calculateArea()}");
+            Console.WriteLine($"PERÍMETRO =  {rectangle.calculatePerimeter().ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"DIAGONAL = {rectangle.calculateDiagonal().ToString("F2", CultureInfo.InvariantCulture)}");
         }
     }
 }
---
+++ PrimeiroProjeto/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿namespace PrimeiroProjeto
+{
+    internal class Rectangle
+    {
+        public double Width;
+        public double Height;
+
+        public double calculateArea()
+        {
+            return this.Width * this.Height;
+        }
+
+        public double calculatePerimeter()
+        {
+            return 2 * (this.Width + this.Height);
+        }
+
+        public double calculateDiagonal()
+        {
+            return Math.Sqrt(Math.Pow(this.Width, 2) + Math.Pow(this.Height, 2));
+        }
+    }
+}
