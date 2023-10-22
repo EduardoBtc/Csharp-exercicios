b007ddc866840ce499b806b6fa41afa267500c87
Eduardo Martins <88164806+EduardoBtc@users.noreply.github.com>
2023-10-22 15:32:43 -0300

    Conhecendo auto-properties

--- PrimeiroProjeto/Product.cs
+++ PrimeiroProjeto/Product.cs
@@ -6,8 +6,8 @@ namespace PrimeiroProjeto
     internal class Product
     {
         private string _name;
-        private double _price;
-        private int _quantity;
+        public double Price {  get; private set; }
+        public int Quantity { get; private set; }
 
         public Product()
         {
@@ -17,14 +17,14 @@ namespace PrimeiroProjeto
         public Product(string name, double price)
         {
             this._name = name;
-            this._price = price;
+            this.Price = price;
         }
 
         public Product(string name, double price, int quantity)
         {
             this._name = name;
-            this._price = price;
-            this._quantity = quantity;
+            this.Price = price;
+            this.Quantity = quantity;
         }
 
         public string Name
@@ -42,43 +42,26 @@ namespace PrimeiroProjeto
 
         }
 
-        public double Price
-        {
-            get 
-            { 
-                return _price; 
-            }
-
-        }
-
-        public int Quantity
-        {
-            get
-            {
-                return _quantity;
-            }
-        }
-
         public double ValorTotalEmEstoque()
         {
-            return _price * _quantity;
+            return Price * Quantity;
         }
 
         public void AdicionarProdutos(int quantity)
         {
-            this._quantity += quantity;
+            this.Quantity += quantity;
         }
 
         public void RemoverProdutos(int quantity)
         {
-            this._quantity -= quantity;
+            this.Quantity -= quantity;
         }
 
         public override string ToString()
         {
             return this._name + ", $ "
-                + this._price.ToString("F2", CultureInfo.InvariantCulture) + ", "
-                + this._quantity
+                + this.Price.ToString("F2", CultureInfo.InvariantCulture) + ", "
+                + this.Quantity
                 + " unidades, Total: $ " 
                 + this.ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
         }
