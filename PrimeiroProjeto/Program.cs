fb2bdb335d94d1f3b477ecdde7b08490f7e26e2d
Eduardo Martins <88164806+EduardoBtc@users.noreply.github.com>
2023-10-23 09:27:15 -0300

    Aprendendo sobre vetores

--- PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/Program.cs
@@ -7,7 +7,38 @@ namespace PrimeiroProjeto
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Quantos quartos serão alugados? ");
+            int numberRooms = int.Parse(Console.ReadLine());
+
+            Room[] rooms = new Room[9];
+
+            for (int i = 1; i <= numberRooms; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}:");
+
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+
+                rooms[quarto] = new Room(name, email, quarto);
+            }
+
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                if (rooms[i] == null)
+                    continue;
+
+                Console.WriteLine();
+                Room currentRoom = rooms[i];
+                Console.WriteLine("Quartos ocupados:");
+                Console.WriteLine($"{i}: {currentRoom}");
+            }
         }
     }
 }
---
+++ PrimeiroProjeto/Room.cs
@@ -0,0 +1,21 @@
+﻿namespace PrimeiroProjeto
+{
+    internal class Room
+    {
+        public string PersonName { get; private set; }
+        public string PersonEmail { get; private set; }
+        public int numberRoom { get; private set; }
+
+        public Room(string personName, string personEmail, int numberRoom)
+        {
+            this.PersonName = personName;
+            this.PersonEmail = personEmail;
+            this.numberRoom = numberRoom;
+        }
+
+        public override string ToString()
+        {
+            return PersonName + ", " + PersonEmail;
+        }
+    }
+}
